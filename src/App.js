6760c6887ca7536ce00fbf131af53bbbf8dbf6ce
IgorNichiporukCristo <inichiporuk1992@gmail.com>
2021-12-19 20:18:09 +0300

    feat:add eslint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react"
+import "./fetch";
 
 function App() {
     return (<div>
+        <fetch />
         <h2>Welcome to React App</h2>
-        <h3>Date : {new Date().toDateString()}</h3>
+        <h3>Date1 : {new Date().toDateString()}</h3>
     </div>)
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  // Where files should be sent once they are bundled
- output: {
-   path: path.join(__dirname, '/dist'),
-   filename: 'index.bundle.js'
- },
-  // webpack 5 comes with devServer which loads in development mode
- devServer: {
-   port: 3000,
-   static: "hot"
- },
-  // Rules of how webpack will take our files, complie & bundle them for the browser 
- module: {
-   rules: [
-     {
-       test: /\.(js|jsx)$/,
-       exclude: /nodeModules/,
-       use: {
-         loader: 'babel-loader'
-       }
-     },
-     {
-       test: /\.css$/,
-       use: [MiniCssExtractPlugin.loader, 'css-loader']
-     }
-   ]
- },
- plugins: [new HtmlWebpackPlugin({ template: './src/index.html' }),new MiniCssExtractPlugin()],
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js'
+  },
+  devServer: {
+    port: 3000,
+    static: "hot"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /nodeModules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', "sass-loader"]
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' }), new MiniCssExtractPlugin()],
 }
