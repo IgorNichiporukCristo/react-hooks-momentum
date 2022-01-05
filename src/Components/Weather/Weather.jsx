01863dea06bb6612fd303cb4b39cb1f78d0c25f3
igor <inichiporuk1992@gmail.com>
2022-01-05 16:47:31 +0300

    feat: update weather block

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -3,39 +3,31 @@ import "./weather.scss";
 import { KEY_WEATHER } from '../../constans';
 
 const Wearther = () => {
-  const [weather, setWeather] = useState({
-    city: "Brest",
-    temperature: 0,
-    // description: "fef",
-    speed: 0,
-    humidity: 0
-  });
+  const [city, setCity] = useState("Brest")
+  const [temperature, setTemperature] = useState(0)
+  const [description, setDescription] = useState("weather")
+  const [speed, setSpeed] = useState(0)
+  const [humidity, setHumidity] = useState(0)
+  
 
 
   useEffect(() => {
     let cleanupFunction = false;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${weather.city}&lang=en&appid=${KEY_WEATHER}&units=metric`;
-     const fetchData = () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=en&appid=${KEY_WEATHER}&units=metric`;
       fetch(url)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.main.humidity)
         if (data.cod == "404" && !cleanupFunction){
           alert("неверно введены данные");
         } else  if(!cleanupFunction){
-          setWeather({...weather, humidity: Math.round(data.main.humidity)})
-          setWeather({...weather, temperature: data.main.temp})
-          // setWeather({...weather, description: data.weather[0].description})
-          setWeather({...weather, speed: data.wind.speed})
-          
+          setTemperature(Math.round(data.main.temp))
+          setDescription(data.weather[0].description)
+          setSpeed( Math.round(data.wind.speed))
+          setHumidity( Math.round(data.main.humidity))
         }
       }) 
-     } 
-    
-     fetchData();
 
-    return () => cleanupFunction = true;
-  },[weather.city]);
+  },[city]);
   
       
 
@@ -43,12 +35,14 @@ const Wearther = () => {
 
   return (
     <div className="weather">
-      <input type="text" className="city" value={weather.city}  onChange={e => setWeather({...weather, city:e.target.value})}/>
+      <input type="text" className="city" value={city}  onChange={e => setCity(e.target.value)}/>
       <div className="description-container">
-        <span className="temperature">{weather.temperature}</span>
-        {/* <span className="weather-description">{weather.description}</span> */}
-        <span className="wind-speed">{weather.speed}</span>
-        <span className="humidity">{weather.humidity}</span>
+        <div className='temperature-description'>
+          <span className="temperature">{temperature}°C</span>
+          <span className="weather-description">{description}</span>
+        </div>
+        <span className="wind-speed">{speed}m/s</span>
+        <span className="humidity">{humidity}%</span>
       </div>
     </div>
   )
